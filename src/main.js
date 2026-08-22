6b4276b87886f6d91915d2871650a3fd4d3028d0
Ilham Putra Arysila <ilhamputraaris22@gmail.com>
2026-08-22 10:28:09 +0700

    project

--- src/main.js
+++ src/main.js
@@ -74,7 +74,7 @@ document.querySelectorAll('.nav-link').forEach((link) => {
 
 // Efek ketik untuk teks headline hero.
 const typingText = document.querySelector('.typing-text');
-const phrases = ['Website Developer', 'Mobile Developer', 'Quality Assurance'];
+const phrases = ['Website Developer', 'Mobile Developer', 'Vibe Coding', 'Quality Assurance'];
 let phraseIndex = 0;
 let charIndex = 0;
 let deleting = false;
